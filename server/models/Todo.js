c9f6563bfadf7c91cb261073e91e3528a0f398b9
DivyanshuPandey1702 <divyanshupandey1702@gmail.com>
2023-03-03 10:34:00 +0530

    Added Files

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -5,11 +5,11 @@ const todoSchema = new mongoose.Schema({
     type: String,
     trim: true,
     required: [true, "A todo name must be provided"],
-    maxlength: [50, "Todo must be at max 200 characters"],
+    maxlength: [200, "Todo must be at max 200 characters"],
   },
   day: {
     type: String,
-    reuired: [true, "A todo must have day and time"],
+    required: [true, "A todo must have day and time"],
   },
   reminder: {
     type: Boolean,
